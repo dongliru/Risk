00c1c01f23313ded7eae7ebc7798592b315574c2
dongliru <787644040@qq.com>
2017-12-13 13:45:30 +0800

    布局和样式基本完成

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,49 +1,69 @@
-$(function(){
-// 左侧柱状图 
-var myChart = echarts.init(document.getElementById("charts-left"));
-var myChart2 = echarts.init(document.getElementById("charts-right"));
+$(function () {
+    // 左侧柱状图 
+    var myChart = echarts.init(document.getElementById("charts-left"));
+    var myChart2 = echarts.init(document.getElementById("charts-right"));
 
-option = {
-    title: {
-        text: '世界人口总量',
-    },
-    tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-            type: 'shadow'
-        }
-    },
-    legend: {
-        data: ['2011年', '2012年']
-    },
-    grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true
-    },
-    xAxis: {
-        type: 'value',
-        boundaryGap: [0, 0.01]
-    },
-    yAxis: {
-        type: 'category',
-        data: ['巴西','印尼','美国','印度','中国','世界人口(万)']
-    },
-    series: [
-        {
+    option = {
+
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'shadow'
+            }
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: [{
+            type: 'value',
+            show: false,
+            boundaryGap: [0, 0.01],
+            splitLine: {
+                show: false, //不显示分割线         
+            },
+        }],
+        yAxis: {
+            type: 'category',
+            data: ['213573', '213572', '213571', '213570', '213569'],
+            splitLine: {
+                show: false, //不显示分割线         
+            },
+        },
+        series: [{
             name: '2011年',
             type: 'bar',
-            data: [18203, 23489, 29034, 104970, 131744, 630230]
-        },
-        {
-            name: '2012年',
-            type: 'bar',
-            data: [19325, 23438, 31000, 121594, 134141, 681807]
-        }
-    ]
-};
-myChart.setOption(option);
-myChart2.setOption(option);
+            data: [18203, 23489, 29034, 104970, 131744],
+            itemStyle: {
+                //通常情况下：
+                normal: { //每个柱子的颜色即为colorList数组里的每一项，如果柱子数目多于colorList的长度，则柱子颜色循环使用该数组
+                    label: {
+                        show: true,
+                        textStyle: {
+                            fontWeight: 'bolder',
+                            fontSize: '12',
+                            fontFamily: '微软雅黑',
+                            color: '#828282'
+                        },
+                        position: 'right'
+                    },
+                    color: function (params) {
+                        var colorList = ['#9fd0fa', '#60a6f5', '#9fd0fa', '#60a6f5', '#3a81f8'];
+                        return colorList[params.dataIndex];
+                    }
+                },
+                //鼠标悬停时：
+                emphasis: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            },
+        }]
+    };
+    myChart.setOption(option);
+    myChart2.setOption(option);
 
 })
